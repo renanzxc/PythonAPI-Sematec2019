3471a14226ce303a989d5e9ac60ec47639d6f4cc
Renan <renanzx@live.com>
2019-10-15 15:56:49 -0300

    Arquivos pessoas py e json

---
+++ pessoas.py
@@ -0,0 +1,31 @@
+#Dicionário
+pessoas = [
+    {
+        "id": 1,
+        "nome": "Renan",
+        "idade": 19,
+        "genero": "M",
+        "contato": "(88) 9 9999-9999"
+    },
+    {
+        "id": 2,
+        "nome": "Maria",
+        "idade": 22,
+        "genero": "F",
+        "contato": "(88) 9 8888-8888"
+    },
+    {
+        "id": 3,
+        "nome": "João",
+        "idade": 32,
+        "genero": "M",
+        "contato": "(88) 9 1111-1111"
+    },
+    {
+        "id": 4,
+        "nome": "Paula",
+        "idade": 20,
+        "genero": "F",
+        "contato": "(88) 9 8888-8888"
+}
+]
